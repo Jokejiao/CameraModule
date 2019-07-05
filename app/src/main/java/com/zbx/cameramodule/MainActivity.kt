f43799132b2f478834d60a73cac9334854375dcc
zbx-jk <jk@vzenith.com>
2019-07-05 09:10:57 +0800

    Implement the preview function
    The client is able to call setPreviewOn and passes on its texture view
    to kick off camera preview

--- app/src/main/java/com/zbx/cameramodule/MainActivity.kt
+++ app/src/main/java/com/zbx/cameramodule/MainActivity.kt
@@ -2,19 +2,27 @@ package com.zbx.cameramodule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.zbx.cameralib.AutoFitTextureView
 import com.zbx.cameralib.CameraManipulator
 
 class MainActivity : AppCompatActivity() {
-    var cameraManipulator: CameraManipulator? = null
+    private var cameraManipulator: CameraManipulator? = null
+    private lateinit var textureView: AutoFitTextureView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textureView = findViewById(R.id.view_preview)
+        findViewById<Button>(R.id.button).setOnClickListener { cameraManipulator?.setPreviewOn(textureView) }
     }
 
     override fun onResume() {
         super.onResume()
-        cameraManipulator = CameraManipulator.Builder().setClientContext(this).build()
+//        cameraManipulator = CameraManipulator.Builder().setClientContext(this).build()
+        cameraManipulator = CameraManipulator.Builder().setClientContext(this).setPreviewOn(textureView).setCameraId(1).build()
         cameraManipulator?.start()
     }
 
